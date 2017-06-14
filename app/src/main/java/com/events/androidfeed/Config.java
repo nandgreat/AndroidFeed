e0d751bb4cf9f19fba01fcf61abaf2dba9ab0192
Nandom <paul4nank@gmail.com>
2017-06-14 15:38:16 +0100

    Initial Comment

---
+++ app/src/main/java/com/events/androidfeed/Config.java
@@ -0,0 +1,14 @@
+package com.events.androidfeed;
+
+/**
+ * Created by Belal on 12/5/2015.
+ */
+public class Config {
+    //Data URL
+    public static final String DATA_URL = "http://192.168.0.107/androidfeed/feed.php?page=";
+
+    //JSON TAGS
+    public static final String TAG_IMAGE_URL = "image";
+    public static final String TAG_NAME = "name";
+    public static final String TAG_PUBLISHER = "publisher";
+}
